785972e9f307bb24867ac417004b66b2d6d5eb3f
Sajjad <sajjad@Sajjads-MacBook-Pro.local>
2025-08-05 19:33:36 +0330

    improve cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -15,36 +15,24 @@ fi
 
 docker compose up zookeeper journalnode1 journalnode2  -d
 
-sleep 10
+sleep 20
 
-docker run --rm -it \
-  -v ./config/core-site.xml:/etc/hadoop/core-site.xml \
-  -v ./config/hdfs-site.xml:/etc/hadoop/hdfs-site.xml \
-  -v HDFSpark_namenode1-data:/hadoop/dfs/name \
-  --network HDFSpark_hadoop-network \
-  --name namenode1 -e HDFS_CONF_dfs_ha_namenode_id=namenode1 \
-  bde2020/hadoop-namenode:2.0.0-hadoop3.2.1-java8 \
-  hdfs namenode -format -force test-ha
+docker compose run --rm namenode1 format
 
 docker compose up namenode1  -d
 
-sleep 10
+# Wait for namenode1 to be ready
+while true; do
+  if curl --silent --fail http://localhost:9870/ >/dev/null 2>&1; then
+    echo "namenode1 is up!"
+    break
+  else
+    echo "Waiting for namenode1..."
+    sleep 2
+  fi
+done
 
-docker run --rm -it \
-  -v ./config/core-site.xml:/etc/hadoop/core-site.xml \
-  -v ./config/hdfs-site.xml:/etc/hadoop/hdfs-site.xml \
-  -v HDFSpark_namenode2-data:/hadoop/dfs/name \
-  --network HDFSpark_hadoop-network \
-  --name namenode2  -e HDFS_CONF_dfs_ha_namenode_id=namenode2 \
-  bde2020/hadoop-namenode:2.0.0-hadoop3.2.1-java8 \
-  hdfs namenode -bootstrapStandby -force
+docker compose run --rm namenode2 standby
 
-docker compose up namenode2 -d
-
-sleep 10
-
-docker compose up datanode1 datanode2
-
-sleep 20
 
 docker compose down
